64b3406b4b7705c629eb2bff7dde3f0c8615d1b2
wrryy <Woj.rydzewski@gmail.com>
2017-12-17 00:22:12 +0100

    Added UI interaction with DB

---
+++ src/view/QueryDB.java
@@ -0,0 +1,129 @@
+package view;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import data.User;
+
+public class QueryDB {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/school?useSSL=false";
+		try (Connection conn = DriverManager.getConnection(url, "root", "coderslab")) {
+			Scanner scan = new Scanner(System.in);
+			menu(scan, conn);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static private String tableName = "User";
+	static private String tableDBName = "user";
+
+	/**Enables interaction with DB user table.
+	 * 
+	 * @param scan
+	 * @param connection
+	 * @throws SQLException
+	 */
+	static void menu(Scanner scan, Connection connection) throws SQLException {
+		boolean check = true;
+		while (check) {
+			showMenu();
+			String choice = scan.next();
+			if (choice.equals("1") || choice.toLowerCase().equals("show")) {
+				System.out.println(MessageFormat.format("{0}s:", tableName));
+				User[] dbRecord = User.loadAll(connection);
+				for (int i = 0; i < dbRecord.length; i++) {
+					System.out.println(dbRecord[i].getId()+ ": " + dbRecord[i].toString());
+				}
+				System.out.println();
+
+			} else if (choice.equals("2") || choice.toLowerCase().equals("add")) {
+				System.out.printf("Enter info about new %s\n", tableName);
+				ArrayList<String> input = getData(connection, scan, 2);
+				User newDbRecord = new User(input.get(0), input.get(2), input.get(1), Integer.parseInt(input.get(3)));
+				newDbRecord.saveToDB(connection);
+
+			} else if (choice.equals("3") || choice.toLowerCase().equals("edit")) {
+				System.out.printf("Enter updated info about %s", tableName);
+				ArrayList<String> input = getData(connection, scan, 1);
+				User updatedDbRecord = User.loadById(connection, Integer.parseInt(input.get(0)));
+				updatedDbRecord.setUsername(input.get(1));
+				updatedDbRecord.setEmail(input.get(2));
+				updatedDbRecord.setPassword(input.get(3));
+				updatedDbRecord.setUserGroupId(Integer.parseInt(input.get(4)));
+				updatedDbRecord.saveToDB(connection);
+			} else if (choice.equals("4") || choice.toLowerCase().equals("delete")) {
+				System.out.printf("Enter %s.id  to delete:", tableName); 
+				User.loadById(connection, intInput0(scan)).delete(connection);
+
+			} else if (choice.equals("5") || choice.toLowerCase().equals("quit")) {
+				System.out.println("Quiting program.");
+				check = false;
+			}
+		}
+	}
+	/**Prints menu options.
+	 * 
+	 */
+	static public void showMenu() {
+		String menu = MessageFormat.format(
+				"Choose option:\n"
+				+ "1. Show all {0}s \n"
+				+ "2. Add {0} \n"
+				+ "3. Edit {0} \n"
+				+ "4. Delete {0} \n"
+				+ "5. Quit",
+				tableName);
+		System.out.println(menu);
+	}
+	/**Returns list of String user data input.
+	 * 
+	 * @param connection
+	 * @param scan
+	 * @param start
+	 * @return ArrayList<String>
+	 * @throws SQLException
+	 */
+	static ArrayList<String> getData(Connection connection, Scanner scan, int start) throws SQLException {
+		Statement statement = connection.createStatement();
+		ResultSet rs = statement.executeQuery("select * from " + tableDBName);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnsNumber = rsmd.getColumnCount();
+		ArrayList<String> inputData = new ArrayList<>();
+		if (rs.next()) {
+			for (int i = start; i <= columnsNumber; i++) {
+				System.out.println("Type in " + rsmd.getColumnName(i) + ":");
+				inputData.add(scan.next());
+			}
+		}
+		return inputData;
+	}
+	/**Returns integer from user input if it meets certain conditions. 
+	 * 
+	 * @param scan
+	 * @return int
+	 */
+	static int intInput0(Scanner scan) {
+		while (!scan.hasNextInt()) {
+			System.out.println("This is not a number!");
+			scan.next();
+		}
+		int integer = scan.nextInt();
+		while (integer <= 0) {
+			System.out.println("Please enter number higher than zero!");
+			intInput0(scan);
+			integer = scan.nextInt();
+		}
+		return integer;
+	}
+}
