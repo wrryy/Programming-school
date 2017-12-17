56727ef61b8cec45963127fcc81f3a59891e6c46
wrryy <Woj.rydzewski@gmail.com>
2017-12-17 12:36:22 +0100

    small changes

--- src/view/QueryDB.java
+++ src/view/QueryDB.java
@@ -15,11 +15,10 @@ import data.User;
 public class QueryDB {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		String url = "jdbc:mysql://localhost:3306/school?useSSL=false";
 		try (Connection conn = DriverManager.getConnection(url, "root", "coderslab")) {
-			Scanner scan = new Scanner(System.in);
 			menu(scan, conn);
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -28,7 +27,8 @@ public class QueryDB {
 	static private String tableName = "User";
 	static private String tableDBName = "user";
 
-	/**Enables interaction with DB user table.
+	/**
+	 * Enables interaction with DB user table.
 	 * 
 	 * @param scan
 	 * @param connection
@@ -43,7 +43,7 @@ public class QueryDB {
 				System.out.println(MessageFormat.format("{0}s:", tableName));
 				User[] dbRecord = User.loadAll(connection);
 				for (int i = 0; i < dbRecord.length; i++) {
-					System.out.println(dbRecord[i].getId()+ ": " + dbRecord[i].toString());
+					System.out.println(dbRecord[i].getId() + ": " + dbRecord[i].toString());
 				}
 				System.out.println();
 
@@ -63,8 +63,8 @@ public class QueryDB {
 				updatedDbRecord.setUserGroupId(Integer.parseInt(input.get(4)));
 				updatedDbRecord.saveToDB(connection);
 			} else if (choice.equals("4") || choice.toLowerCase().equals("delete")) {
-				System.out.printf("Enter %s.id  to delete:", tableName); 
-				User.loadById(connection, intInput0(scan)).delete(connection);
+				System.out.printf("Enter %s.id  to delete:", tableName);
+				User.loadById(connection, intInputCheck0(scan)).delete(connection);
 
 			} else if (choice.equals("5") || choice.toLowerCase().equals("quit")) {
 				System.out.println("Quiting program.");
@@ -72,21 +72,19 @@ public class QueryDB {
 			}
 		}
 	}
-	/**Prints menu options.
+
+	/**
+	 * Prints menu options.
 	 * 
 	 */
 	static public void showMenu() {
-		String menu = MessageFormat.format(
-				"Choose option:\n"
-				+ "1. Show all {0}s \n"
-				+ "2. Add {0} \n"
-				+ "3. Edit {0} \n"
-				+ "4. Delete {0} \n"
-				+ "5. Quit",
-				tableName);
+		String menu = MessageFormat.format("Choose option:\n" + "1. Show all {0}s \n" + "2. Add {0} \n"
+				+ "3. Edit {0} \n" + "4. Delete {0} \n" + "5. Quit", tableName);
 		System.out.println(menu);
 	}
-	/**Returns list of String user data input.
+
+	/**
+	 * Returns list of String user data input.
 	 * 
 	 * @param connection
 	 * @param scan
@@ -108,22 +106,22 @@ public class QueryDB {
 		}
 		return inputData;
 	}
-	/**Returns integer from user input if it meets certain conditions. 
+
+	/**
+	 * Returns integer from user input if it meets certain conditions.
 	 * 
-	 * @param scan
+	 * @param scanner
 	 * @return int
 	 */
-	static int intInput0(Scanner scan) {
-		while (!scan.hasNextInt()) {
-			System.out.println("This is not a number!");
-			scan.next();
-		}
-		int integer = scan.nextInt();
-		while (integer <= 0) {
-			System.out.println("Please enter number higher than zero!");
-			intInput0(scan);
-			integer = scan.nextInt();
-		}
-		return integer;
+	static int intInputCheck0(Scanner scanner) {
+		int number;
+		do {
+			while (!scanner.hasNextInt()) {
+				System.out.println("That's not a number!");
+				scanner.next(); // this is important!
+			}
+			number = scanner.nextInt();
+		} while (number <= 0);
+		return number;
 	}
 }
